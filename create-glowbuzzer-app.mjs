035b7a3100ac880d076087a6195b7e16de993a88
jugglingcats <github@akirkpatrick.com>
2023-07-26 13:12:40 +0100

    fix: fix issue with version handling

--- create-glowbuzzer-app.mjs
+++ create-glowbuzzer-app.mjs
@@ -13,7 +13,7 @@ const force = args.includes("--force");
 noversion && console.log("Using latest development version of GBR");
 force && console.log("Force overwrite existing project enabled");
 
-function make_url(version) {
+function package_fetch_url(version) {
     return `https://cdn.jsdelivr.net/gh/glowbuzzer/gbr@${version}/package.json`;
 }
 
@@ -44,13 +44,22 @@ async function fetch(url) {
     return response.data;
 }
 
+async function versions() {
+    if (noversion) {
+        console.log("Creating project using latest development template and alpha GBR version ... please wait")
+        return ["alpha", "main"]
+    } else {
+        const {version}=await fetch(package_fetch_url("main"));
+        console.log(`Creating project using GBR version '${version}' ... please wait`);
+        return [version, "v"+version];
+    }
+}
+
 try {
     // fetch package.json from gbr repository in order to determine current release version
-    const version=noversion ? "main" : "v"+(await fetch(make_url("main"))).version
+    const [npm_version, github_version] = await versions();
 
-    console.log(`Creating project using GBR version '${version}' ... please wait`);
-
-    const templateRepo = `glowbuzzer/gbr/template#${version}`;
+    const templateRepo = `glowbuzzer/gbr/template#${github_version}`;
 
     console.log("- Fetching GBR template");
     const emitter = degit(templateRepo, {cache: false, force: true});
@@ -59,8 +68,8 @@ try {
     // fetch the specific version of the package.json from the GBR repository,
     // so that we can update dependencies to specific versions used by the main project
     console.log("- Updating dependency versions");
-    const versioned = await fetch(make_url(version));
-    process_project(answers.projectName, version, versioned);
+    const versioned = await fetch(package_fetch_url(github_version));
+    process_project(answers.projectName, npm_version, versioned);
 
     console.log(`\nYour Glowbuzzer React project has been created in '${answers.projectName}'\n`);
     console.log("To get started:\n    cd " + answers.projectName + "\n    npm install\n    npm start\n");

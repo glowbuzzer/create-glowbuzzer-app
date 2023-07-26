0fb5fb98c5a8931f8ad26026b715899de1e9760d
jugglingcats <github@akirkpatrick.com>
2023-07-26 12:31:20 +0100

    chore: change to jsdelivr fix: don't overwrite existing projects

--- create-glowbuzzer-app.mjs
+++ create-glowbuzzer-app.mjs
@@ -1,12 +1,17 @@
 #!/usr/bin/env node
 
+import {existsSync} from "fs";
 import degit from 'degit';
 import inquirer from "inquirer";
-import {fetch} from "./src/fetch.mjs"
 import {process_project} from "./src/process.mjs";
+import axios from "axios";
 
 const [, , arg] = process.argv;
-const noversion=arg === "--noversion";
+const noversion = arg === "--noversion";
+
+function make_url(version) {
+    return `https://cdn.jsdelivr.net/gh/glowbuzzer/gbr@${version}/package.json`;
+}
 
 const questions = [
     {
@@ -19,22 +24,43 @@ const questions = [
 
 const answers = await inquirer.prompt(questions);
 
-// fetch package.json from gbr repository in order to determine current release version
-const gbr= await fetch('https://raw.githubusercontent.com/glowbuzzer/gbr/master/package.json');
-const version= gbr.version;
+// don't overwrite an existing project
+if (existsSync(answers.projectName)) {
+    console.log("Error: directory '" + answers.projectName + "' already exists");
+    process.exit(1)
+}
 
-console.log(`Creating project using GBR version ${version} ... please wait`);
+async function fetch(url) {
+    const response = await axios.get(url, {
+        headers: {
+            'Cache-Control': 'no-cache',
+            'Accept': 'application/json'
+        }
+    });
+    return response.data;
+}
 
-const templateRepo = noversion ? "glowbuzzer/gbr/template" : `glowbuzzer/gbr/template#v${version}`;
+try {
+    // fetch package.json from gbr repository in order to determine current release version
+    const gbr = await fetch(make_url("master"));
+    const version = gbr.version;
 
-const emitter = degit(templateRepo, {cache: false, force: true});
-await emitter.clone(answers.projectName);
+    console.log(`Creating project using GBR version ${version} ... please wait`);
 
-// fetch the specific version of the package.json from the GBR repository,
-// so that we can update dependencies to specific versions used by the main project
-const versioned= await fetch(`https://raw.githubusercontent.com/glowbuzzer/gbr/v${version}/package.json`);
+    const templateRepo = noversion ? "glowbuzzer/gbr/template" : `glowbuzzer/gbr/template#v${version}`;
 
-process_project(answers.projectName, version, versioned);
+    console.log("- Fetching GBR template");
+    const emitter = degit(templateRepo, {cache: false, force: true});
+    await emitter.clone(answers.projectName);
 
-console.log(`\nYour Glowbuzzer React project has been created in '${answers.projectName}'\n`);
-console.log("To get started:\n    cd " + answers.projectName + "\n    npm install\n    npm start\n");
+    // fetch the specific version of the package.json from the GBR repository,
+    // so that we can update dependencies to specific versions used by the main project
+    console.log("- Updating dependency versions");
+    const versioned = await fetch(make_url(version));
+    process_project(answers.projectName, version, versioned);
+
+    console.log(`\nYour Glowbuzzer React project has been created in '${answers.projectName}'\n`);
+    console.log("To get started:\n    cd " + answers.projectName + "\n    npm install\n    npm start\n");
+} catch (e) {
+    console.log("Error: " + e.message);
+}
